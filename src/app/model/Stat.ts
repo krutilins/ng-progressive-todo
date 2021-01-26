42750ec60751b5bb21832886b16174060404469d
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-01-26 12:13:49 +0300

    add stat model

---
+++ src/app/model/Stat.ts
@@ -0,0 +1,11 @@
+export class Stat {
+  title: string;
+  icon: string;
+  description: string;
+
+  constructor(title: string, icon: string, description: string) {
+    this.title = title;
+    this.icon = icon;
+    this.description = description;
+  }
+}
